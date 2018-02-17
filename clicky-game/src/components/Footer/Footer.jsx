c872eab20b72258dc93dd627ae2bc8b179c6149d
Joshua Spears <j.dspears@yahoo.com>
2018-02-17 09:46:41 -0800

    Footer component built.

--- clicky-game/src/components/Footer/Footer.jsx
+++ clicky-game/src/components/Footer/Footer.jsx
@@ -13,10 +13,13 @@ const Footer = ({ children }) => {
     // RETURN THE HTML FOR THE FOOTER    
     return (  
         //FOOTER
-        <footer class="footer">
-            <div class="container">
-                <p class="text-muted">
-                    Place sticky footer content here.
+        <footer className="footer text-center">
+            <div className="container">
+                <p className="col-xs-6">
+                    ©Clicker
+                </p>
+                <p className="col-xs-6 col-sm-6 col-md-6">
+                    Joshua Spears 
                 </p>
             </div>
         </footer>
